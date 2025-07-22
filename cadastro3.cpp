4e466c23d783a553a18f585ddb23884ee4de7f56
alexandre soares <53378983+allesoares@users.noreply.github.com>
2025-07-21 22:06:20 -0300

    Opção de saida
    Incluso no sistema a opção de saída para o usuário.

--- cadastro3.cpp
+++ cadastro3.cpp
@@ -128,6 +128,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n\n");  
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: ");//fim menu
 	
 		scanf("%d", &opcao); // armazenando escolha do usuario
@@ -146,6 +147,11 @@ int main()
 				
 			case 3:
 				deletar();
+				break;
+				
+			case 4:
+				printf("Obrigado por usar o sistema!\n");
+				return 0;
 				break;
 				
 				
